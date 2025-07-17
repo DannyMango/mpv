f8ef66ecd6279ceb3efd49069cfa0aad13330504
Anshu Kumar <142654865+DannyMango@users.noreply.github.com>
2025-07-17 12:56:45 +0530

    Delete vs directory

--- vs/histogram/classic.py
+++
@@ -1,6 +0,0 @@
-from vstools import core, depth
-
-g = globals()
-
-if 'video_in' in g:
-    core.hist.Classic(depth(video_in, 8)).set_output()  # type:ignore[name-defined]
--- vs/histogram/color2.py
+++
@@ -1,6 +0,0 @@
-from vstools import core, depth
-
-g = globals()
-
-if 'video_in' in g:
-    core.hist.Color2(depth(video_in, 8)).set_output()  # type:ignore[name-defined]
